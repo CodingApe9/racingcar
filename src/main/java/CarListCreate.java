94860e354638483d4f36423598b4a49446de659c
CodingApe9 <pos06098@gmail.com>
2022-11-07 12:42:43 +0900

    수정 v1.0

--- src/main/java/CarListCreate.java
+++ src/main/java/CarListCreate.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class CarListCreate {
     /**
      * @param inputNameString 차 이름 스트링 ", "로 구분
-     * @return 차 이름 스트링 리스트(null은 에러)
+     * @return 차 이름 스트링 리스트
      */
     public List<String> createList(String inputNameString) throws Exception {
         List<String> carNameList = List.of(inputNameString.split(", "));
+
         if (!isCollectCarName(inputNameString, carNameList)){
             throw new Exception("ㅎㅎ 범인 나오세요");
         }
@@ -22,6 +23,7 @@ public class CarListCreate {
         Set<String> carNameCount = new HashSet<>(names);
 
         if (carNameCount.size() != names.size()){
+            System.out.println("자동차 이름 중복");
             return false;
         }
         //오류 방출
@@ -34,11 +36,12 @@ public class CarListCreate {
                 System.out.println("자동차 이름 길이가 너무 길거나 짧습니다.");
                 return false;
             }
-            if (inputNameString.chars().filter(i -> i==',').count()!=carNameCount.size()) {
-                System.out.println("쉼표 개수와 자동차 개수가 맞지 않음");
-                return false;
-            }
             // TODO : 중복된 자동차 이름 찾기, 쉼표의 개수+1와 자동차 이름의 개수가 맞아야한다.
+        }
+
+        if (inputNameString.chars().filter(i -> i==',').count()+1!=carNameCount.size()) {
+            System.out.println("쉼표 개수와 자동차 개수가 맞지 않음");
+            return false;
         }
         return true;
     }
